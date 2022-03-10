b9123708f838354a107246260f8fae900699164c
dmbldr <58942852+dmbldr@users.noreply.github.com>
2022-03-10 19:54:52 +0300

    solve more problems

---
+++ leetcode/letter-case-permutation.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> letterCasePermutation(string s) {
+        vector<string> result;
+        permutation(result, s, 0);
+        return result;
+    }
+    
+    void permutation(vector<string>& result, string& s, int k)
+    {
+        if(k == s.size()) 
+        {
+            result.push_back(s);
+            return;
+        }
+        while(k < s.size() && '0' <= s[k] && s[k] <= '9') 
+        {
+            ++k;
+        }
+        if(k == s.size()) 
+        {
+            result.push_back(s);
+            return;
+        }
+        
+        s[k] = tolower(s[k]);
+        permutation(result, s, k + 1);
+        s[k] = toupper(s[k]);
+        permutation(result, s, k + 1);
+        s[k] = tolower(s[k]);
+    }
+};
