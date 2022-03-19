cc5b63619d47ca14431145b3a3eb3344350cfe27
dmbldr <58942852+dmbldr@users.noreply.github.com>
2022-03-19 16:02:45 +0300

    Update maximum-subarray.cpp

--- leetcode/maximum-subarray.cpp
+++ leetcode/maximum-subarray.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans = nums[0], sum = 0;
-
-        for(int i = 0; i < nums.size(); ++i)
+        int ans = nums[0];
+        for(int i = 1; i < nums.size(); ++i)
         {
-            sum += nums[i];
-            ans = max(ans, sum);
-            sum = max(sum, 0);
+            nums[i] = max(nums[i - 1] + nums[i], nums[i]);
+            ans = max(ans, nums[i]);
         }
-
         return ans;
     }
 };
